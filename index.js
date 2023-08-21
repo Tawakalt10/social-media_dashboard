355ac566c6751ccb479df01693d06d7fa8e7355a
unknown <Tawakalt10>
2023-08-21 09:15:24 +0100

    new

---
+++ index.js
@@ -0,0 +1,11 @@
+// var icon = document.getElementByTagName('body');
+// var darkMode = document.getElementById('icon');
+// darkMode.addEventListener('click', fun ction(){
+
+// })
+
+function myFunction (){
+    var element = document.body;
+
+element.classList.toggle("dark-mode-colours")
+}
